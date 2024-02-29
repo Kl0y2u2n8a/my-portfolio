8790056f589e5ebbb9ac665818b612a32e123e10
Kl0y2u2n8a <kevinliang228@gmail.com>
2024-03-01 01:42:55 +0900

    fix navbar

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import robot from '../public/robot.png';
 import {useState} from 'react';
 import { FaGripLinesVertical } from "react-icons/fa6";
-import { MyNavbar } from "./navbar.js";
+import MyNavbar from "./navbar";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
@@ -20,38 +20,7 @@ export default function Home() {
         </Head>
         
         <main className="px-5w-auto md:px-10 lg:px-20 bg-gradient-to-tr from-sky-300 via-indigo-300 to-blue-100 dark:from-blue-950 dark:via-indigo-950 dark:to-black">
-          {/* <nav className="py-5 mb-12 flex justify-between sticky top-0 z-50 ml-1">
-            <h1 className="text-xl font-burtons text-blue-900 hover:text-blue-700 dark:text-white dark:hover:text-neutral-300"><a href='/'>kevinliangweb</a></h1>
-            <ul className="flex items-center ml-2">
-              
-              <li><button 
-                className="font-burtons text-[10px] align-middle md:text-sm ml-1 md:ml-2 lg:ml-4 text-blue-900 hover:text-blue-700 dark:text-white  dark:hover:text-neutral-300">
-                Skills</button></li>
-
-              <li className="font-burtons text-sm align-text-bottom md:text-xl ml-1 md:ml-2 lg:ml-4 text-blue-900 dark:text-white"><FaGripLinesVertical /></li>  
-
-              <li><button 
-                className="font-burtons text-[10px] align-middle md:text-sm ml-1 md:ml-2 lg:ml-4 text-blue-900 hover:text-blue-700 dark:text-white dark:hover:text-neutral-300">
-                Work Experience</button></li>
-              <li className="font-burtons text-sm align-text-bottom md:text-xl ml-1 md:ml-2 lg:ml-4 text-blue-900 dark:text-white"><FaGripLinesVertical /></li>
-
-              <li><button 
-                className="font-burtons text-[10px] align-middle md:text-sm ml-1 md:ml-2 lg:ml-4 text-blue-900 hover:text-blue-700 dark:text-white  dark:hover:text-neutral-300">
-                Projects</button></li>
-
-              <li className="font-burtons text-sm align-text-bottom md:text-xl ml-1 md:ml-2 lg:ml-4 text-blue-900 dark:text-white"><FaGripLinesVertical /></li>
-
-              <li><BsFillMoonStarsFill onClick={()=> setDarkMode(!darkMode)} className=" cursor-pointer  text-sm md:text-xl ml-1 md:ml-2 lg:ml-4
-                  text-blue-900 hover:text-blue-700 dark:text-white dark:hover:text-neutral-300"/></li>
-              
-              <li className="font-burtons text-sm md:text-md ml-1 md:ml-2 lg:ml-4 text-blue-900 dark:text-white"><FaGripLinesVertical /></li>
-              
-              <li><button className="border-2 border-gray-600 rounded-full bg-gradient-to-r from-blue-500 to-sky-400 px-2 justify-center 
-                                    ml-2 lg:ml-4 font-burtons text-l text-blue-900 dark:text-white hover:bg-gradient-to-l dark:hover:text-neutral-300">Resume</button></li>
-            </ul>
-          </nav> */}
           <MyNavbar/>
-
           
           <section className="min-h-screen">
             <div className="text-center p-10 ">
--- pages/navbar.js
+++ pages/navbar.js
@@ -3,63 +3,65 @@ import { BsFillMoonStarsFill } from 'react-icons/bs';
 import { FaGripLinesVertical } from "react-icons/fa6";
 
 
-export const MyNavbar = () => {
-    <nav className="py-5 mb-12 flex justify-between sticky top-0 z-50 ml-1">
-        <h1 className="text-xl font-burtons text-blue-900 hover:text-blue-700 dark:text-white dark:hover:text-neutral-300">
-            <a href="/">kevinliangweb</a>
-        </h1>
-        <ul className="flex items-center ml-2">
-            <li>
-                <button className="font-burtons text-[10px] align-middle md:text-sm ml-1 md:ml-2 lg:ml-4 text-blue-900 hover:text-blue-700 dark:text-white  dark:hover:text-neutral-300">
-                    Skills
-                </button>
-            </li>
+const MyNavbar = () => {
+    return (
 
-            <li className="font-burtons text-sm align-text-bottom md:text-xl ml-1 md:ml-2 lg:ml-4 text-blue-900 dark:text-white">
-                <FaGripLinesVertical />
-            </li>
+        <nav className="py-5 mb-12 flex justify-between sticky top-0 z-50 ml-1">
+            <h1 className="text-xl font-burtons text-blue-900 hover:text-blue-700 dark:text-white dark:hover:text-neutral-300">
+                <a href="/">kevinliangweb</a>
+            </h1>
+            <ul className="flex items-center ml-2">
+                <li>
+                    <button className="font-burtons text-[10px] align-middle md:text-sm ml-1 md:ml-2 lg:ml-4 text-blue-900 hover:text-blue-700 dark:text-white  dark:hover:text-neutral-300">
+                        Skills
+                    </button>
+                </li>
 
-            <li>
-                <button className="font-burtons text-[10px] align-middle md:text-sm ml-1 md:ml-2 lg:ml-4 text-blue-900 hover:text-blue-700 dark:text-white dark:hover:text-neutral-300">
-                    Work Experience
-                </button>
-            </li>
-            <li className="font-burtons text-sm align-text-bottom md:text-xl ml-1 md:ml-2 lg:ml-4 text-blue-900 dark:text-white">
-                <FaGripLinesVertical />
-            </li>
+                <li className="font-burtons text-sm align-text-bottom md:text-xl ml-1 md:ml-2 lg:ml-4 text-blue-900 dark:text-white">
+                    <FaGripLinesVertical />
+                </li>
 
-            <li>
-                <button className="font-burtons text-[10px] align-middle md:text-sm ml-1 md:ml-2 lg:ml-4 text-blue-900 hover:text-blue-700 dark:text-white  dark:hover:text-neutral-300">
-                    Projects
-                </button>
-            </li>
+                <li>
+                    <button className="font-burtons text-[10px] align-middle md:text-sm ml-1 md:ml-2 lg:ml-4 text-blue-900 hover:text-blue-700 dark:text-white dark:hover:text-neutral-300">
+                        Work Experience
+                    </button>
+                </li>
+                <li className="font-burtons text-sm align-text-bottom md:text-xl ml-1 md:ml-2 lg:ml-4 text-blue-900 dark:text-white">
+                    <FaGripLinesVertical />
+                </li>
 
-            <li className="font-burtons text-sm align-text-bottom md:text-xl ml-1 md:ml-2 lg:ml-4 text-blue-900 dark:text-white">
-                <FaGripLinesVertical />
-            </li>
+                <li>
+                    <button className="font-burtons text-[10px] align-middle md:text-sm ml-1 md:ml-2 lg:ml-4 text-blue-900 hover:text-blue-700 dark:text-white  dark:hover:text-neutral-300">
+                        Projects
+                    </button>
+                </li>
 
-            <li>
-                <BsFillMoonStarsFill
-                    onClick={() => setDarkMode(!darkMode)}
-                    className=" cursor-pointer  text-sm md:text-xl ml-1 md:ml-2 lg:ml-4
-                text-blue-900 hover:text-blue-700 dark:text-white dark:hover:text-neutral-300"
-                />
-            </li>
+                <li className="font-burtons text-sm align-text-bottom md:text-xl ml-1 md:ml-2 lg:ml-4 text-blue-900 dark:text-white">
+                    <FaGripLinesVertical />
+                </li>
 
-            <li className="font-burtons text-sm md:text-md ml-1 md:ml-2 lg:ml-4 text-blue-900 dark:text-white">
-                <FaGripLinesVertical />
-            </li>
+                <li>
+                    <BsFillMoonStarsFill
+                        onClick={() => setDarkMode(!darkMode)}
+                        className=" cursor-pointer  text-sm md:text-xl ml-1 md:ml-2 lg:ml-4
+            text-blue-900 hover:text-blue-700 dark:text-white dark:hover:text-neutral-300"
+                    />
+                </li>
 
-            <li>
-                <button
-                    className="border-2 border-gray-600 rounded-full bg-gradient-to-r from-blue-500 to-sky-400 px-2 justify-center 
-                                ml-2 lg:ml-4 font-burtons text-l text-blue-900 dark:text-white hover:bg-gradient-to-l dark:hover:text-neutral-300"
-                >
-                    Resume
-                </button>
-            </li>
-        </ul>
-    </nav>
-};
+                <li className="font-burtons text-sm md:text-md ml-1 md:ml-2 lg:ml-4 text-blue-900 dark:text-white">
+                    <FaGripLinesVertical />
+                </li>
 
-export default MyNavbar;
+                <li>
+                    <button
+                        className="border-2 border-gray-600 rounded-full bg-gradient-to-r from-blue-500 to-sky-400 px-2 justify-center 
+                            ml-2 lg:ml-4 font-burtons text-l text-blue-900 dark:text-white hover:bg-gradient-to-l dark:hover:text-neutral-300"
+                    >
+                        Resume
+                    </button>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+export default MyNavbar
